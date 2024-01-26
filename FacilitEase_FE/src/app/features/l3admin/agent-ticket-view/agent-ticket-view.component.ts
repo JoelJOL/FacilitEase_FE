64c5f917ba7a30f5f7da6c6182ef2807d8cb8a87
Julia Johny Puthenpurayil <julia.puthenpurayil@experionglobal.local>
2024-01-26 10:56:53 +0530

    integrated resolve and forward to manager functionality

--- FacilitEase_FE/src/app/features/l3admin/agent-ticket-view/agent-ticket-view.component.ts
+++ FacilitEase_FE/src/app/features/l3admin/agent-ticket-view/agent-ticket-view.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
 import { AgentService } from '../../service/httpService/agent.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { ModalService } from '@app/features/service/dataService/modal.service';
+import { ModalComponent } from '@app/components/layout/modal/modal.component';
+import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
 
 @Component({
   selector: 'app-agent-ticket-view',
@@ -9,24 +11,44 @@ import { ModalService } from '@app/features/service/dataService/modal.service';
   styleUrls: ['./agent-ticket-view.component.css']
 })
 export class AgentTicketViewComponent { 
-
   customHeaderText = 'Supported Attachments';  
   ticketId: number = 0; 
-  ticketDetails: any=[]; 
-  constructor(private route: ActivatedRoute,private agentService: AgentService, private myModalService: ModalService) {} 
+  ticketDetails: any = []; 
+  modalRef: BsModalRef | undefined;
+
+  constructor(private route: ActivatedRoute, private agentService: AgentService, private modalService: BsModalService, private router: Router) {} 
+
   ngOnInit(): void { 
     this.route.params.subscribe((params) => { 
       this.ticketId = Number(params['id']); 
       console.log(this.ticketId); 
-
     }); 
+
     this.agentService.getData(this.ticketId).subscribe(data => { 
-      this.ticketDetails =data[0]; 
-       console.log(data); 
-  }); 
-  }
-  openModal(): void {
-    this.myModalService.openModal();
+      this.ticketDetails = data[0]; 
+      console.log(data); 
+    }); 
   }
 
-  } 
+  openModal(ticketDetails: any) {
+    this.modalRef = this.modalService.show(ModalComponent, {
+      initialState: {
+        ticketDetails: ticketDetails
+      }
+    });
+  }
+
+  resolveTicket(): void {
+    this.agentService.resolveTicket(this.ticketId).subscribe(
+      (response) => {
+        console.log('API call success:', response);
+        alert("Ticket resolved successfully!");
+        this.router.navigate(['/view-ticket']); 
+      },
+      (error) => {
+        console.error('API call error:', error);
+
+      }
+    );
+  }
+}
