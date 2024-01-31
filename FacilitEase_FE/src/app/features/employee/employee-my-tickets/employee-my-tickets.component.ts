8f2b7c2c5bc60e8d12ead55f2fdabbf69df678e3
Hema K Shaji <hema.shaji@experionglobal.local>
2024-01-31 19:14:42 +0530

    comp:request to cancel and navigation

--- FacilitEase_FE/src/app/features/employee/employee-my-tickets/employee-my-tickets.component.ts
+++ FacilitEase_FE/src/app/features/employee/employee-my-tickets/employee-my-tickets.component.ts
@@ -4,20 +4,24 @@ import { DropDownService } from '@app/features/service/httpService/dropdown.serv
 @Component({
   selector: 'app-employee-my-tickets',
   templateUrl: './employee-my-tickets.component.html',
-  styleUrls: ['./employee-my-tickets.component.css']
+  styleUrls: ['./employee-my-tickets.component.css'],
 })
 export class EmployeeMyTicketsComponent {
-  constructor(private masterService :DropDownService){}
-  headers: string[] = [ 'Ticket Name', 'Ticket Description', 'Assigned To', 'Priority', 'Status'];
-  
-  apiLink : string = '' ;
+  constructor(private masterService: DropDownService) {}
+  headers: string[] = [
+    'Ticket ID',
+    'Ticket Name',
+    'Ticket Description',
+    'Assigned To',
+    'Priority',
+    'Status',
+  ];
+
+  apiLink: string = '';
   onRowClicked(rowId: any) {
     console.log('Row clicked in parent component with ID:', rowId);
-    
   }
   ngOnInit(): void {
-    const userId = 1; 
-    const queryParams = new URLSearchParams({ sortField: 'Id' });
-    this.apiLink = `${this.masterService.getMyTicketsBaseUrl()}?userId=${userId}&${queryParams.toString()}`;
+    this.apiLink = `${this.masterService.getMyTickets()}`;
   }
 }
