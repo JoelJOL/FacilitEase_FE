82044583d803de29be793912867f558ae06493a0
Joel <kaduppasseryj28@gmail.com>
2024-02-03 18:13:17 +0530

    Authentication frontend

--- FacilitEase_FE/src/app/app.module.ts
+++ FacilitEase_FE/src/app/app.module.ts
@@ -110,6 +110,20 @@ import { TicketDetailViewNoeditComponent } from './components/layout/ticket-deta
 import { OnHoldTicketsViewComponent } from './features/l3admin/on-hold-tickets-view/on-hold-tickets-view.component';
 import { OutsideClickDirective } from './features/service/directive/outside-click/outside-click.directive';
 import { ReportStatsComponent } from './components/ui_elements/report-stats/report-stats.component';
+import { LoginScreenComponent } from './features/Authentication/login-screen/login-screen.component';
+import {
+  MsalGuard,
+  MsalInterceptor,
+  MsalModule,
+  MsalRedirectComponent,
+} from '@azure/msal-angular';
+import { PublicClientApplication, InteractionType } from '@azure/msal-browser';
+import { AzureService } from './features/Authentication/azureService/azure.service';
+
+const isIE =
+  window.navigator.userAgent.indexOf('MSIE') > -1 ||
+  window.navigator.userAgent.indexOf('Trident/') > -1;
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -196,6 +210,8 @@ import { ReportStatsComponent } from './components/ui_elements/report-stats/repo
     TicketDetailViewNoeditComponent,
     OnHoldTicketsViewComponent,
     ReportStatsComponent,
+    OutsideClickDirective,
+    LoginScreenComponent,
   ],
   imports: [
     HttpClientModule,
@@ -216,6 +232,32 @@ import { ReportStatsComponent } from './components/ui_elements/report-stats/repo
     MatIconModule,
     ToastrModule.forRoot(),
     MatCardModule,
+    MsalModule.forRoot(
+      new PublicClientApplication({
+        auth: {
+          clientId: 'd7104f84-ab29-436f-8f06-82fcf8d81381',
+          redirectUri: 'http://localhost:4200',
+          authority:
+            'https://login.microsoftonline.com/5b751804-232f-410d-bb2f-714e3bb466eb',
+        },
+        cache: {
+          cacheLocation: 'localStorage',
+          storeAuthStateInCookie: isIE,
+        },
+      }),
+      {
+        interactionType: InteractionType.Redirect,
+        authRequest: {
+          scopes: ['user.read'],
+        },
+      },
+      {
+        interactionType: InteractionType.Redirect,
+        protectedResourceMap: new Map([
+          ['https://graph.microsoft.com/v1.0/me', ['user.Read']],
+        ]),
+      }
+    ),
   ],
   providers: [
     FormServiceService,
@@ -224,7 +266,14 @@ import { ReportStatsComponent } from './components/ui_elements/report-stats/repo
     EmployeeBulkuploadService,
     ModalService,
     MasterService,
+    // {
+    //   provide: HTTP_INTERCEPTORS,
+    //   useClass: MsalInterceptor,
+    //   multi: true,
+    // },
+    MsalGuard,
+    AzureService,
   ],
-  bootstrap: [AppComponent],
+  bootstrap: [AppComponent, MsalRedirectComponent],
 })
 export class AppModule {}
