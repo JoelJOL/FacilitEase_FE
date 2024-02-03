016da520d9c1dd4a10fc396e04941ab964579a7c
Julia Johny Puthenpurayil <julia.puthenpurayil@experionglobal.local>
2024-02-03 18:09:37 +0530

    Added no records

--- FacilitEase_FE/src/app/app-routing.module.ts
+++ FacilitEase_FE/src/app/app-routing.module.ts
@@ -52,6 +52,8 @@ import { DetailsEscalatedComponent } from './features/l2admin/details-escalated/
 import { ManagerViewTicketSimpleComponent } from './features/manager/manager-view-ticket-simple/manager-view-ticket-simple.component';
 import { ResolvedTicketViewComponent } from './features/l3admin/resolved-ticket-view/resolved-ticket-view.component';
 import { OnHoldTicketsViewComponent } from './features/l3admin/on-hold-tickets-view/on-hold-tickets-view.component';
+import { CancelRequestViewAllComponent } from './features/l3admin/cancel-request-view-all/cancel-request-view-all.component';
+import { CancelRequestViewComponent } from './features/l3admin/cancel-request-view/cancel-request-view.component';
 
 const routes: Routes = [
   {
@@ -102,11 +104,14 @@ const routes: Routes = [
     path: 'l3',
     component: L3adminComponent,
     children: [
+      { path: '', redirectTo: 'view-ticket', pathMatch: 'full' },
       { path: 'view-ticket', component: AgentTicketsViewComponent },
       { path: 'resolved-tickets', component: ResolvedTicketsViewComponent },
       { path: 'on-hold-tickets', component: OnHoldTicketsViewComponent },
       { path: 'view-ticket-in-detail/:Id', component: AgentTicketViewComponent },
       { path: 'view-ticket-detail-noedit/:Id', component: ResolvedTicketViewComponent},
+      { path:'cancel-requests', component:CancelRequestViewAllComponent},
+      { path:'request-to-cancel-detail/:Id', component:CancelRequestViewComponent},
       { path: 'l2admin-subordinates', component: L2adminSubordinatesComponent },
       // { path: 'lreport/:id', component: L2ReportComponent },
     ],
