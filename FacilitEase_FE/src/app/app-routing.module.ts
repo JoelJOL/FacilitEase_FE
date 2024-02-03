8093b2f7c56f6c091074535d33fd5150e10617e6
Avinash K R <avinash.kr@experionglobal.local>
2024-02-03 18:16:25 +0530

    day 3 feb

--- FacilitEase_FE/src/app/app-routing.module.ts
+++ FacilitEase_FE/src/app/app-routing.module.ts
@@ -52,6 +52,7 @@ import { DetailsEscalatedComponent } from './features/l2admin/details-escalated/
 import { ManagerViewTicketSimpleComponent } from './features/manager/manager-view-ticket-simple/manager-view-ticket-simple.component';
 import { ResolvedTicketViewComponent } from './features/l3admin/resolved-ticket-view/resolved-ticket-view.component';
 import { OnHoldTicketsViewComponent } from './features/l3admin/on-hold-tickets-view/on-hold-tickets-view.component';
+import { EmplycarddisplayComponent } from './components/layout/emplycarddisplay/emplycarddisplay.component';
 
 const routes: Routes = [
   {
@@ -105,8 +106,14 @@ const routes: Routes = [
       { path: 'view-ticket', component: AgentTicketsViewComponent },
       { path: 'resolved-tickets', component: ResolvedTicketsViewComponent },
       { path: 'on-hold-tickets', component: OnHoldTicketsViewComponent },
-      { path: 'view-ticket-in-detail/:Id', component: AgentTicketViewComponent },
-      { path: 'view-ticket-detail-noedit/:Id', component: ResolvedTicketViewComponent},
+      {
+        path: 'view-ticket-in-detail/:Id',
+        component: AgentTicketViewComponent,
+      },
+      {
+        path: 'view-ticket-detail-noedit/:Id',
+        component: ResolvedTicketViewComponent,
+      },
       { path: 'l2admin-subordinates', component: L2adminSubordinatesComponent },
       // { path: 'lreport/:id', component: L2ReportComponent },
     ],
@@ -229,8 +236,8 @@ const routes: Routes = [
         component: ReactiveFormComponent,
       },
       {
-        path: 'file-upload',
-        component: FileUploadComponent,
+        path: 'emplycarddisplay',
+        component: EmplycarddisplayComponent,
       },
     ],
   },
