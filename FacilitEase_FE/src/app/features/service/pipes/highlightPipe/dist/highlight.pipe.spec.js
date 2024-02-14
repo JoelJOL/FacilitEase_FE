6cc61e0a81aaf680edc2299c591099245fb439c1
abhijith.nair <abhikannan51@gmail.com>
2024-02-14 17:13:21 +0530

    added test cases

---
+++ FacilitEase_FE/src/app/features/service/pipes/highlightPipe/dist/highlight.pipe.spec.js
@@ -0,0 +1,8 @@
+"use strict";
+exports.__esModule = true;
+describe('HighlightPipe', function () {
+    // it('create an instance', () => {
+    //   const pipe = new HighlightPipe();
+    //   expect(pipe).toBeTruthy();
+    // });
+});
