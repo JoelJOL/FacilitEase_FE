1ebdb8cdbdaa3b3d267858bff23491434579ec0d
abhijith.nair <abhijith.nair@experionglobal.com>
2024-03-27 11:05:50 +0530

    Added comment while editing Sla for ticket

--- FacilitEase_FE/src/app/features/l2admin/components/sla-edit-modal/sla-edit-modal.component.ts
+++ FacilitEase_FE/src/app/features/l2admin/components/sla-edit-modal/sla-edit-modal.component.ts
@@ -1,8 +1,11 @@
 // sla-edit-modal.component.ts
-import { Component, Inject, Input } from '@angular/core';
+import { Component, EventEmitter, Inject, Input, Output } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { SlaEditServiceService } from '@app/features/service/httpService/slaEditService/sla-edit-service.service';
 import { HttpResponse } from '@angular/common/http';
+import { CommentService } from '@app/features/service/httpService/commentService/comment.service';
+import { CommentInterface } from '@app/comment-interface';
+import { ActiveCommentInterface } from '@app/active-comment-interface';
 
 @Component({
   selector: 'app-sla-edit-modal',
@@ -15,11 +18,14 @@ export class SlaEditModalComponent {
   newExpectedDate: Date = new Date();
   comment: string = '';
   ticketId !: number;
+  comments: CommentInterface[] = [];
+  activeComment: ActiveCommentInterface | null = null;
 
   constructor(
     public dialogRef: MatDialogRef<SlaEditModalComponent>,
     @Inject(MAT_DIALOG_DATA) public data: any,
-    private slaEditService: SlaEditServiceService
+    private slaEditService: SlaEditServiceService,
+    private commentService: CommentService
   ) {
     this.ticketId = data.ticketId;
   }
@@ -42,12 +48,17 @@ export class SlaEditModalComponent {
 
   onSaveClick(): void {
     // Calculate new expected date by adding days
-    this.newExpectedDate = new Date(this.data);
+    console.log(this.data);
+    this.newExpectedDate = new Date(this.data.resolvingTime);
     this.newExpectedDate.setDate(this.newExpectedDate.getDate() + parseInt(this.daysToAdd.toString(), 10));
     console.log(this.newExpectedDate);
-    this.comment = `The expected date to resolve this ticket has been changed to ${this.newExpectedDate} because ${this.comment}`
+    this.comment = `The expected date to resolve this ticket has been changed to ${this.newExpectedDate.toString()}. Reason-: ${this.comment}`
     console.log(this.comment);
-
+    this.commentService.addComment(this.comment,null,this.ticketId,2)
+    .subscribe((createdComment) => {
+      this.comments = [...this.comments, createdComment];
+      this.activeComment = null;
+    });
     this.slaEditService.editTicketSLA(this.ticketId, this.daysToAdd).subscribe(
       (response : HttpResponse<any>) => {
         console.log('API response:', response);
