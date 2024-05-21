350d56c6e9ae9217b9fca5a55e41cc6d0b229188
Julia Johny Puthenpurayil <julia.puthenpurayil@experionglobal.com>
2024-05-21 14:45:54 +0530

    modifying the no edit ticket detailed view

--- FacilitEase_FE/src/app/components/layout/ticket-detail-view-noedit/ticket-detail-view-noedit.component.ts
+++ FacilitEase_FE/src/app/components/layout/ticket-detail-view-noedit/ticket-detail-view-noedit.component.ts
@@ -8,6 +8,9 @@ import { AzureService } from '@app/features/Authentication/azureService/azure.se
 })
 export class TicketDetailViewNoeditComponent {
   customHeaderText = '';
+  selectedTab: string = 'Details'; // Tab Selection Variable
+  userEmail: string = "joel.jose@experionglobal.com" // Email ID for chat
+
   currentUserId: number = this.azureService.userId;
   constructor(private azureService: AzureService) {
     console.log('This is the ID', this.currentUserId);
@@ -18,4 +21,11 @@ export class TicketDetailViewNoeditComponent {
   }
   @Input() ticketDetails: any;
   @Input() headings: any[] = [];
+
+  // Method to open the teams chat when button is clicked
+  goToTeams() {
+    let n = "Hey " + "\nCongratulations on being a fool 🎉...";
+    location.href = "MSTeams:/l/chat/0/0?users=" + this.userEmail + "&message= " + n;
+  }
 }
+
